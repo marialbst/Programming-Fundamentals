8513bd56484843ea14a07d9cc778cf8ce931b909
marialbst <marialbst@gmail.com>
2016-09-08 22:47:35 +0300

    lab & excersices
    added files from homeworks

---
+++ first-excersices/InstructionSet/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class InstructionSet_broken
+{
+    static void Main()
+    {
+        string opCode = Console.ReadLine();
+        decimal result = 0;
+
+        while (opCode.ToLower() != "end")
+        {
+            string[] codeArgs = opCode.Split(' ');
+
+            
+            switch (codeArgs[0])
+            {
+                case "INC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        operandOne++;
+                        result = operandOne;
+                        break;
+                    }
+                case "DEC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        operandOne--;
+                        result = operandOne;
+                        break;
+                    }
+                case "ADD":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        long operandTwo = long.Parse(codeArgs[2]);
+                        result = operandOne + operandTwo;
+                        break;
+                    }
+                case "MLA":
+                    {
+                        decimal operandOne = decimal.Parse(codeArgs[1]);
+                        decimal operandTwo = decimal.Parse(codeArgs[2]);
+                        result = operandOne * operandTwo;
+                        break;
+                    }
+            }
+            opCode = Console.ReadLine();
+            Console.WriteLine(result);
+        }
+       
+    }
+}
---
+++ first-excersices/SequenceOfCommands/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+
+public class SequenceOfCommands_broken
+{
+    private const char ArgumentsDelimiter = ' ';
+
+    public static void Main()
+    {
+        int sizeOfArray = int.Parse(Console.ReadLine());
+
+        long[] array = Console.ReadLine()
+            .Split(ArgumentsDelimiter)
+            .Select(long.Parse)
+            .ToArray();
+
+        string command = Console.ReadLine();
+
+        while (!command.Equals("stop"))
+        {
+            string line = command.Trim();
+            int[] args = new int[2];
+            string[] stringParams = line.Split(ArgumentsDelimiter);
+            command = stringParams[0];
+
+            if (command.Equals("add") ||
+                command.Equals("subtract") ||
+                command.Equals("multiply"))
+            {
+                args[0] = int.Parse(stringParams[1]);
+                args[1] = int.Parse(stringParams[2]);
+                PerformAction(array, command, args);
+            }
+            else
+            {
+                PerformAction(array, command, args);
+            }
+            PrintArray(array);
+            Console.WriteLine();
+
+            command = Console.ReadLine();
+        }
+    }
+
+    static void PerformAction(long[] array, string action, int[] args)
+    {
+        int pos = args[0];
+        int value = args[1];
+
+
+        switch (action)
+        {
+            case "multiply":
+                array[pos - 1] *= value;
+                break;
+            case "add":
+                array[pos - 1] += value;
+                break;
+            case "subtract":
+                array[pos - 1] -= value;
+                break;
+            case "lshift":
+                ArrayShiftLeft(array);
+                break;
+            case "rshift":
+                ArrayShiftRight(array);
+                break;
+        }
+    }
+
+    private static void ArrayShiftRight(long[] array)
+    {
+        long indexLast = array[array.Length - 1];
+
+        for (int i = array.Length - 1; i >= 1; i--)
+        {
+            array[i] = array[i - 1];
+
+        }
+
+        array[0] = indexLast;
+    }
+
+    private static void ArrayShiftLeft(long[] array)
+    {
+        long indexFirst = array[0];
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        array[array.Length - 1] = indexFirst;
+
+
+    }
+
+    private static void PrintArray(long[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
