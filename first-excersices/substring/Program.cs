d27aa439934bbd62e796f8fd45b6583fce4e6180
marialbst <marialbst@gmail.com>
2016-09-08 22:54:10 +0300

    changed code

--- first-excersices/substring/Program.cs
+++ first-excersices/substring/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace substring
+public class Substring_broken
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string text = Console.ReadLine();
+        int jump = int.Parse(Console.ReadLine());
+
+        const char Search = 'p';
+        bool hasMatch = false;
+
+        for (int i = 0; i < text.Length; i++)
         {
+            if (text[i] == Search)
+            {
+                hasMatch = true;
+
+                int endIndex = jump + 1;// = lenght of substring
+
+                string matchedString = "";
+                if (endIndex + i > text.Length)
+                {
+                    matchedString = text.Substring(i);
+                }
+                else
+                {
+                    matchedString = text.Substring(i, endIndex);
+                }
+
+                Console.WriteLine(matchedString);
+                i += jump;
+            }
+        }
+
+        if (!hasMatch)
+        {
+            Console.WriteLine("no");
         }
     }
 }
