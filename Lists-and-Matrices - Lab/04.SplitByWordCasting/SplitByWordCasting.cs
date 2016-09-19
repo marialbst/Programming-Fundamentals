c1f8e4615c13badfca2adfede1c762e489723f03
marialbst <marialbst@gmail.com>
2016-09-20 00:00:56 +0300

    update

--- Lists-and-Matrices - Lab/04.SplitByWordCasting/SplitByWordCasting.cs
+++ Lists-and-Matrices - Lab/04.SplitByWordCasting/SplitByWordCasting.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _04.SplitByWordCasting
+namespace SplitByWordCasting
 {
-    class Program
+    class SplitByWordCasting
     {
         static void Main(string[] args)
         {
+            string[] words = Console.ReadLine().Trim().Split(new Char[] { ' ', ';', ':', ',', '.', '!', '(', ')', '"', '\'', '\\', '/', '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> uppercased = new List<string>();
+            List<string> lowercased = new List<string>();
+            List<string> mixed = new List<string>();
+            long num;
+            
+            foreach (string word in words)
+            {
+                if (word == word.ToLower() && word.ToCharArray().All(c => char.IsLetter(c)))
+                {
+                    lowercased.Add(word);
+                }
+                else if (word == word.ToUpper() && word.ToCharArray().All(c => char.IsLetter(c)))
+                {
+                    uppercased.Add(word);
+                }
+                else
+                {
+                    mixed.Add(word);
+                }
+            } 
+           
+            Console.WriteLine("Lower-case: {0}", string.Join(", ", lowercased));
+            Console.WriteLine("Mixed-case: {0}", string.Join(", ", mixed));
+            Console.WriteLine("Upper-case: {0}", string.Join(", ", uppercased));
         }
     }
 }
