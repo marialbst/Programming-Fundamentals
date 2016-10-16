b23932783a3fd04bc4289e965bcf8a111c0104fe
marialbst <marialbst@gmail.com>
2016-10-16 22:02:16 +0300

    up

---
+++ Programming-fundamentals-old-exams/04.PopulationAggregation/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.PopulationAggregation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Dictionary<string, Dictionary<string, int>> countries = new Dictionary<string, Dictionary<string, int>>();
+            while (!command.Equals("stop"))
+            {
+                string[] data = command.Split('\\');
+                string city = "";
+                string country = "";
+                int population = int.Parse(data[2]);
+                for (int i=0;i<2;i++)
+                {
+                    data[i] =
+                        data[i].Split(new char[] {'@', '$', '&', '#', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'},
+                            StringSplitOptions.RemoveEmptyEntries).ToString();
+                }
+
+                if (data[0].StartsWith(data[0].ToLower()))
+                {
+                    city = data[0];
+                    country = data[1];
+                }
+                else
+                {
+                    city = data[1];
+                    country = data[0];
+                }
+
+                if (!countries.ContainsKey(country))
+                {
+                    countries.Add(country, new Dictionary<string, int>());
+                }
+
+                if (!countries[country].ContainsKey(city))
+                {
+                    countries[country].Add(city, 0);
+                }
+
+                countries[country][city] = population;
+                command = Console.ReadLine();
+            }
+
+            foreach (var country in countries)
+            {
+                Console.WriteLine($"{country.Key} -> {country.Value.Keys.Count}");
+            }
+        }
+    }
+
+    
+}
--- Programming-fundamentals-old-exams/04.PopulationAggregation/Properties/AssemblyInfo.cs
+++ Programming-fundamentals-old-exams/04.PopulationAggregation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("04.PopulationAggregation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("04.PopulationAggregation")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6dae31a6-7ddf-48ff-a164-8c559e249f8e")]
+[assembly: Guid("529167a4-6524-4d5a-bad0-f5ba21ec46f9")]
 
 // Version information for an assembly consists of the following four values:
 //
