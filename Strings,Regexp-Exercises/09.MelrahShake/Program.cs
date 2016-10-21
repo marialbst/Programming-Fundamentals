6e47eb32b5eec182c6cc0ae2327a62c9017e44db
Maria Stoeva <marialbst@users.noreply.github.com>
2016-10-21 10:28:10 +0300

    Update Program.cs

--- Strings,Regexp-Exercises/09.MelrahShake/Program.cs
+++ Strings,Regexp-Exercises/09.MelrahShake/Program.cs
@@ -6,37 +6,36 @@ namespace _09.MelrahShake
     {
         string text = Console.ReadLine();
         string pattern = Console.ReadLine();
-		int firstIndex = text.IndexOf(pattern, StringComparison.Ordinal);
-        int lastIndex = text.LastIndexOf(pattern, StringComparison.Ordinal);
+	
         while (true)
         {
-			if (firstIndex >= 0 && lastIndex > firstIndex + pattern.Length-1 && lastIndex > 0)
-            {
-                text = text.Remove(lastIndex, pattern.Length );
-				text = text.Remove(firstIndex, pattern.Length);
-				Console.WriteLine("Shaked it.");
-                    
-         	}
-			else 
-			{
-				Console.WriteLine("No shake.");
-				break;
-			}
-
-			pattern = pattern.Remove(pattern.Length / 2, 1);
-				
-			if(pattern.Length == 0)
-			{
-				Console.WriteLine("No shake.");
-				break;
-			}
-			else
-			{
-				firstIndex = text.IndexOf(pattern, StringComparison.Ordinal);
-				lastIndex = text.LastIndexOf(pattern, StringComparison.Ordinal);
-			}
+		int firstIndex = text.IndexOf(pattern, StringComparison.Ordinal);
+        	int lastIndex = text.LastIndexOf(pattern, StringComparison.Ordinal);
+		if (firstIndex >= 0 && lastIndex > firstIndex + pattern.Length-1 && lastIndex > 0)
+            	{
+			text = text.Remove(lastIndex, pattern.Length );
+			text = text.Remove(firstIndex, pattern.Length);
+			Console.WriteLine("Shaked it.");
 		}
-		Console.WriteLine(text);
-        }
-    }
+		else 
+		{
+			Console.WriteLine("No shake.");
+			break;
+		}
+
+		pattern = pattern.Remove(pattern.Length / 2, 1);
+				
+		if(pattern.Length == 0)
+		{
+			Console.WriteLine("No shake.");
+			break;
+		}
+		else
+		{
+			firstIndex = text.IndexOf(pattern, StringComparison.Ordinal);
+			lastIndex = text.LastIndexOf(pattern, StringComparison.Ordinal);
+		}
+	}
+	Console.WriteLine(text);
 }
+
